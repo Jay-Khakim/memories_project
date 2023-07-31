2bdaf721705428aa6955b05b82da729188adf2a2
Jay-Khakim <mgmediajay@gmail.com>
2023-07-31 19:25:46 +0900

    working on controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,10 @@
+import PostMessage from "../models/postMEssage.js"
+
 
 export const getPosts = (req, res) => {
     res.send('This WORKS')
+}
+
+export const createPost = (req, res) =>{
+    res.send('Post Creation ')
 }
